2ed57bbdc65bb013bf036c5c7d767d9192068b10
Christian Hansen <68639582+christian-hansen@users.noreply.github.com>
2023-04-24 19:44:41 +0200

    js opt

--- js/script.js
+++ js/script.js
@@ -156,3 +156,27 @@ function showLegalEN() {
     document.getElementById('legalenglish').classList.remove('d-none');
     document.getElementById('legalgerman').classList.add('d-none');
 }
+
+/**
+ * If the login menu has the class 'd-none', remove it. Otherwise, add it
+ */
+function subMenu() {
+    let menu = document.getElementById('logMenue');
+    
+    if (menu.classList.contains('d-none')) {
+        menu.classList.remove('d-none');
+    } else {
+        menu.classList.add('d-none');
+    }
+}
+
+
+/**
+ * It returns a string of HTML that contains two buttons, one that cancels the deletion of a task, and one that deletes the task.
+ * @param i - the index of the task in the array
+ * @returns A string of HTML.
+ */
+function renderContactDeleteBtns(i) {
+    return `<div>Are you sure you want to delete this contact?</div>
+    <div class="deletemsgbtns"><div onclick="cancelContactDeletion()">No</div><div class="alerthover" onclick="deleteContact(${i})">Yes</div></div>`
+  }
